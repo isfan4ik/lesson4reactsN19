aba751d166958d3f31282cf9abe181b7dd05e06d
isfan4ik <isfandiyorprogrammer@gmail.com>
2023-08-26 08:10:51 -0700

    lesson 4

---
+++ main.js
@@ -0,0 +1,585 @@
+//  let day = 8;
+
+//  switch (day) {
+//       case 1:
+//       console.log(du);
+//       break;
+//       case 2:
+//       console.log(se);  
+//       break;
+//       case 3: 
+//       console.log(cho);
+//       break;
+//       case 4:
+//       console.log(pay);
+//       break;
+//       case 7:
+//       console.log(ya);
+//       break;
+//       default:
+//       console.log('not found');
+//       break;
+//  }
+
+
+// class Car {
+//    constructor(name,model,weight,speed){
+//       this.name = name;
+//       this.model = model;
+//       this.weight = weight;
+//       this.speed = speed;
+//    }
+   
+//    getInfo(){
+//       console.log(
+//          this.name + this.model
+//       );
+//    }
+
+// }
+
+
+
+// class Gentra extends Car{
+//    constructor(name,model,weight,speed,abs,airback){
+//       super(name,model,weight,speed)
+//       this.abs = abs;
+//       this.airback = airback;
+//    }
+// }
+
+// let Gentra = new Daewoo("Gentra",4,'1.5 tonna',220,true,true);
+
+
+// let Gentra = new Car("Gentra",3,'1.8 tonna',200);
+
+
+// class Phone {
+//    constructor(name,model,weight,speed){
+//       this.name = name;
+//       this.model = model;
+//       this.weight = weight;
+//       this.speed = speed;
+//    }
+
+//    getInfo(){
+//       console.log(
+//          this.name + this.model
+//       );
+//    }
+// }
+
+// let HuaweiY7 =  new Phone("HuaweiY7",2019,'450gramm',0);
+
+
+
+
+
+// let vaqt = new Date();
+
+// let havta = vaqt.getDate();
+
+// let kun = vaqt .getDate();
+// let soat = vaqt .getHours();
+// let min = vaqt .getMinutes();
+
+
+
+// ---------------------------------------------------------------------------------------HOMEWORK-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+// 1
+
+// let a = 5;
+
+// switch (jumla) {
+//    case 1:
+//       console.log(jumla);
+//       break;
+//       case 2:
+//          console.log(butun);
+//          break;
+// }
+
+
+// 2
+
+// let a = 6;
+
+// switch (toq) {
+//    case 1:
+//       console.log(jumla);
+//       break;
+//    case 2:
+//       console.log(toq);
+//       break;
+// }
+
+
+// 3
+
+// let a = 9;
+
+// switch (a) {
+//    case 1:
+//       console.log(butun);
+//       break;
+//    case 2:
+//       console.log(juft);
+//       break;
+// }
+
+
+// 4
+
+// let a = 5;
+// let b = 6;
+
+// switch (M-s a > 0 && b > 0) {
+//    case 1:
+//       console.log(butun);
+//       break;
+//       console.log(a < b);
+//       break;
+// }
+
+// 5 
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a > b < c) {
+//    case 1:
+//       console.log(a > b);
+//       break;
+//    case 2:   
+//       console.log(b < c);
+//       break;
+//    case 3:
+//       console.log(a > 5);
+//       break;
+// }
+
+
+// 6 
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a > b < c) {
+//    case 1:
+//       console.log(a > b);
+//       break;
+//    case 2:   
+//       console.log(b < c);
+//       break;
+//    case 3:
+//       console.log(a > 5);
+//       break;
+// }
+
+
+
+// 7 
+
+// let a = 5;
+// let b = 6;
+
+// switch (a == b) {
+//    case 1:
+//       console.log(butun);
+//       break;
+//       console.log(a < b);
+//       break;
+// }
+
+
+// 8 
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+// let d = 10;
+
+// switch (a,b,c.d) {
+//    case 1:
+//       console.log(a);
+//       break;
+//    case 2:   
+//       console.log(b);
+//       break;
+//    case 3:
+//       console.log(c);
+//       break;
+//    case 4:
+//       console.log(d);
+//       break;
+// }
+
+// 9
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a,b) {
+//    case 1:
+//       console.log(a);
+//       break;
+//    case 2:   
+//       console.log(b);
+//       break;
+//    case 3:
+//       console.log(c);
+//       break;
+// }
+
+// 10
+
+// let vaqt = new Date();
+
+// let havta = vaqt.getDate();
+
+// let du = vaqt .getDate();
+// let se = vaqt.getDate();
+// let cho = vaqt .getDate();
+// let pay = vaqt .getDate();
+// let juma = vaqt .getDate();
+// let sha = vaqt .getDate();
+// let yak = vaqt .getDate();
+
+// 11
+
+// let = 110
+
+// switch (110) {
+//    case 1:
+//    console.log(110);
+//    break;
+// }
+
+
+// 12
+
+
+// let = 5;
+
+// switch (5-1) {
+//    case 1:
+//       console.log(5-1);
+//       break;
+// }
+
+// 13
+
+// let = 5;
+
+// switch (5+3-2) {
+//    case 1:
+//       console.log(5+3-2);
+//       break;
+// }
+
+// 14
+
+// let a = 6;
+// let b = 9;
+
+// switch (a > b) {
+//    case value:
+//       console.log(a < b);
+//       break;
+// }
+
+// 15
+
+// let a = 15;
+// let b = 20;
+// let c = 2;
+
+// switch (a < 20 > 2) {
+//    case value:
+//       console.log(b);
+//       break;
+// }
+
+// 16
+
+// let a = 15;
+// let b = 20;
+// let c = 2;
+
+// switch (a < b > c) {
+//    case value:
+//       console.log(c);
+//       break;
+// }
+
+// 17
+
+// 18
+
+
+// let a = 15;
+// let b = 20;
+// let c = 2;
+
+// switch (a < 20 > 2) {
+//    case value:
+//       console.log(b);
+//       break;
+// }
+
+// 19
+
+// let a = 15;
+// let b = 15;
+// switch (true) {
+//     case a % 2 == 0 && b % 2 == 0:
+//         console.log('ikkisi ham juft');
+//         break;
+//     case a % 2 != 0 && b % 2 == 0:
+//         console.log('a soni toq');
+//         break;
+//     case a % 2 == 0 && b % 2 != 0:
+//         console.log('b soni toq');
+//         break;
+//     case a % 2 != 0 && b % 2 != 0:
+//         console.log('ikkisi ham toq');
+//         break;
+//     default:
+//         console.log('bu son emas');
+//         break;
+// }
+
+// 20
+
+// let num = -5;
+
+// switch (true) {
+//     case num > 0:
+//         console.log(++num);
+//         break;
+//     case num < 0:
+//         console.log(++num);
+//         break;
+//     default:
+//         console.log ('son kiriting');
+//         break;
+// }
+
+// 21
+
+
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a,b,c) {
+//    case 1:
+//       console.log(a);
+//       break;
+//    case 2:   
+//       console.log(b);
+//       break;
+//    case 3:
+//       console.log(c);
+//       break;
+// }
+
+
+
+// 22
+
+// let a =10;
+// let b =30;
+
+// switch (true) {
+//    case 1:
+//       console.log(30 < 10);
+//       break;
+// }
+
+// 23
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a,b,c) {
+//    case 1:
+//       console.log(a);
+//       break;
+//    case 2:   
+//       console.log(b);
+//       break;
+//    case 3:
+//       console.log(c);
+//       break;
+// }
+
+
+// 24
+
+// let a = 9;
+// let b = 6;
+// let c = 5;
+
+// switch (a,b,c) {
+//    case 1:
+//       console.log(b);
+//       break;
+//    case 2:   
+//       console.log(c);
+//       break;
+//    case 3:
+//       console.log(a);
+//       break;
+// }
+
+
+// 25
+
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// switch (1,2,3) {
+//    case 1:
+//       console.log(1);
+//       break;
+//    case 2:   
+//       console.log(2);
+//       break;
+//    case 3:
+//       console.log(3);
+//       break;
+// }
+
+
+// 26
+
+// let  = 2;
+// let  = 3;
+// let  = 4;
+
+// switch (1,3,4) {
+//    case 1:
+//       console.log(1);
+//       break;
+//    case 2:   
+//       console.log(2);
+//       break;
+//    case 3:
+//       console.log(3);
+//       break;
+// }
+
+
+// 27
+
+// 28
+
+// let = 'mac';
+// switch (laptop) {
+//     case 'asus':
+//         console.log('2000$');
+//         break;
+//     case 'lenevo':
+//         console.log('200$');
+//         break;
+//     case 'hp':
+//         console.log('950$');
+//         break;
+//     case 'mac':
+//         console.log('1000$');
+//         break;
+//     default:
+//         console.log(false);
+//         break;
+// }
+
+/////////////////////////////////////////////////////////////CLASS///////////////////////////////////////////////////////////////////////
+
+// class Car {
+//    constructor(name,model,weight,speed){
+//       this.name = name;
+//       this.model = model;
+//       this.weight = weight;
+//       this.speed = speed;
+//    }
+   
+//    getInfo(){
+//       console.log(
+//          this.name + this.model
+//       );
+//    }
+
+// }
+
+
+
+// class Gentra extends Car{
+//    constructor(name,model,weight,speed,abs,airback){
+//       super(name,model,weight,speed)
+//       this.abs = abs;
+//       this.airback = airback;
+//    }
+// }
+
+// let Gentra = new Daewoo("Gentra",4,'1.5 tonna',220,true,true);
+
+
+// let Gentra = new Car("Gentra",3,'1.8 tonna',200);
+
+
+// class Phone {
+//    constructor(name,model,weight,speed){
+//       this.name = name;
+//       this.model = model;
+//       this.weight = weight;
+//       this.speed = speed;
+//    }
+
+//    getInfo(){
+//       console.log(
+//          this.name + this.model
+//       );
+//    }
+// }
+
+// let HuaweiY7 =  new Phone("HuaweiY7",2019,'450gramm',0);
+
+
+
+// class PC {
+//    constructor(name,model,weight,speed,abs,airback){
+//       super(name,model,weight,speed)
+//       this.abs = abs;
+//       this.airback = airback;
+//    }
+// }
+
+// let PC = new PC("Pc",4,'1,5kg',0,true,true);
+
+
+// let PC = new PC("Pc",3,' 2kg ',0);
+
+// class book extends{
+//    constructor(name,model,weight,speed,abs,airback){
+//       super(name,model,weight,speed)
+//       this.abs = abs;
+//       this.airback = airback;
+//    }
+// }
+
+// let book = new Daewoo("book",4,'200 gramm',220,true,true);
